672d1b58e5521e3da2cb9f42d71591d4a708e64a
TheWireMonkey <thewiremonkey@gmail.com>
2018-05-02 17:35:32 -0700

    week five

--- week_5/tt5.R
+++ week_5/tt5.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(acs)
-library(corrplot)
-library(ggrepel)
 library(noncensus)
 library(magrittr)
+library(readxl)
+library(maps)
+library(ghibli)
+library(RColorBrewer)
 
 here::here()
 url<-"https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/acs2015_county_data.csv"
@@ -11,127 +13,87 @@ url<-"https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/
 dat<-read_csv(url) %>%
   janitor::clean_names()
 
-summary(dat)
+county_map<-map_data('county')
+state_map<-map_data('state')
 
-dat <- dat[complete.cases(dat), ]
+pal<-RColorBrewer::brewer.pal(n = 9,name = "Set1")
 
 data("states")
-states %<>%mutate_at(c("area", "population"), as.numeric) %>%
-  mutate_if(is.factor, as.character) %>%
-  mutate(pop_dens=population/area,
-         division=case_when(
-    state %in% c("AS", "GU", "MP") ~ "Pacific Territory",
-    state %in% c("PR", "VI") ~ "Caribbean Territory",
-    TRUE ~ region
-  ))
-
 data("counties")
-counties%<>%mutate_if(is.factor, as.character)
+counties %<>% mutate(census_id=paste(as.character(state_fips), as.character(county_fips),sep=""))
 
-all<-left_join(states, counties, by="state") %>%
-  select(-CSA, -CBSA) %>%
-  mutate(s_pop=population.x, c_pop=population.y, county=gsub(" County| Parish| Municipio|  Municipality, County", "", county_name)) %>%
-  left_join(dat, by=c("name"="state", "county")) %>%
-  select(-population.x, -population.y)
-
-division<-all %>% group_by(division) %>%
-  summarise(n_counties=n(),dens=mean(pop_dens, na.rm=T), commute=mean(mean_commute, na.rm=T))
-
-p<-ggplot(division, aes(label=division))+
-  geom_text(aes(x=dens, y=commute))
-
-g<-ggplot(all)
-
-gse<-g+geom_point(aes(x=self_employed, y=mean_commute))
-
-dens<-all %>% filter(pop_dens !=max(pop_dens)) %>%
-  group_by(state) %>%
-  summarise(semp=mean(self_employed, na.rm=T),
-            dens=mean(pop_dens, na.rm=T)) %>%
-  filter(!is.nan(semp))
+#data from: https://www.census.gov/support/USACdataDownloads.html#LND
+#Metafile from: www2.census.gov/prod2/statcomp/usac/excel/Mastdata.xls
+LND01 <- read_excel("week_5/LND01.xls") %>% select(STCOU,LND110210D,Areaname) %>%
+  rename(census_id=STCOU)
 
 
+dat <- dat[complete.cases(dat), ]
+dat <-dat %>%
+    mutate(census_id=as.character(census_id))%>%
+  mutate(chars=nchar(census_id))%>%
+  mutate(census_id=case_when(
+    chars==4 ~ paste0("0", census_id, sep=""),
+    chars==5 ~ census_id,
+    TRUE ~ census_id
+  ))%>%
+  left_join(LND01)
 
-sts<-all %>% select(-c(county_name, county_fips, county)) %>% distinct() %>%
-  group_by(state) %>%
-  summarise_if(.predicate = is.numeric, mean, na.rm=T) %>%
-  filter(!is.nan(c_pop)) %>%
-  ungroup() %>%
-  gather(key=variable, value=value, 2:40) %>%
-  filter(!is.na(value))
+gg_dat<-dat %>% select(census_id,state, county, area=LND110210D, pop=total_pop, drive, carpool, transit, walk, other_transp, work_at_home, mean_commute,native) %>%
+  mutate(dens=pop/area, region=tolower(state), subregion=tolower(county)) %>%
+  # filter(!is.na(dens)) %>%
+  left_join(states %>% select(abbr=state,state=name,  division)) %>%
+  mutate_if(is.factor, as.character())
+
+#get division names
+div_names<-unique(gg_dat$division)[1:9]
+#bind div names to pal to ensure  consistency
+names(pal)<-div_names
+
+all_map<-county_map %>%
+  left_join(gg_dat, by=c("region", "subregion"))
+
+ggplot()+
+  geom_polygon(data = all_map, aes(x=long, y=lat, group=group, fill=division), color=NA)+
+  geom_polygon(data=state_map,aes(x=long, y=lat, group=group), fill=NA, color="black")+
+  theme_void()+
+  scale_fill_manual(values=pal)+
+  labs(title="Region Reference, or The Importance of Consistency in Place Names", subtitle="NA are counties whose names do not match between map_data('county') and the given data set", caption="viz by @WireMonkey\ncounty data: ACS 2015 County Data\nland area: USACS LND table\ntopic selection: @ThomasMock")
 
 
-# ##---- old stuff---
-#
-# dat2<-dat %>%
-#   select_if(.,is.numeric) %>%
-#   colMeans() %>%
-#   as.tibble() %>%
-#   mutate(cols= row.names(.)) %>%
-#   filter(value <=100)
-#
-# dat3<-dat %>% select(state, county, dat2$cols)
-#
-#
-# corrplot(cor(dat[,4:37]),"shade",type = "lower", is.corr = FALSE)
-#
-# ##using https://chasemc.github.io/post/tidy-tuesday-4-17-2018/ to learn how to do PCA
-# ##
-# dat4<-dat %>% mutate(id=paste0(state,county,sep=""), men=(men/total_pop)*100, women=(women/total_pop)*100) %>% sample_n(50)
-#
-#
-#
-# a2 <- dat4 %>% group_by(id) %>%
-#            summarize_if(is.numeric,mean)
-#
-#
-# a <- a2 %>% select(4:36) %>% replace(is.na(.), 0)
-#
-#
-#
-# principleComponents <- prcomp(a) %>%
-#   .$x %>%
-#   as_tibble %>%
-#   bind_cols(name=a2$id, .)
-#
-# ggplot(principleComponents) +
-#   geom_label(aes(x=PC1,y=PC2,label=name)) +
-#   ggtitle("First and Second Principle Components")
-#
-# principleComponents2 <- prcomp(a) %>%
-#   .$rotation %>%
-#   as_tibble %>%
-#   bind_cols(name=colnames(a), .)
-#
-# ggplot(principleComponents2) +
-#   geom_label(aes(x=PC1,y=PC2
-#                  ,label=name)) +
-#   ggtitle("First and Second Principle Components, Variable Contributions")
-#
-# unoriginal<-dat %>% group_by(county) %>%
-#   count() %>%
-#   filter(n>1)
-#
-# original<-dat %>% group_by(county) %>%
-#   count() %>%
-#   filter(n==1)
-#
-# sum(unoriginal$n)
-#
-# un<-dat %>%
-#   group_by(state) %>%
-#   filter(county %in% unoriginal$county) %>%
-#   summarise(u=n()) %>%
-#   left_join(dat %>%
-#               group_by(state) %>%
-#               filter(!county %in% unoriginal$county) %>%
-#               summarise(o=n())
-#             )%>%
-#   mutate(perc=u/(o+u), t=o+u)
-#
-#
-#
-# tot<-dat %>%
-#   split(.$state) %>%
-#   map(~nrow(.))
-# ##----New Stuff
+# p<-ggplot(gg_dat, aes(x=dens))
+# p1<-p+
+#   geom_point(aes(y=mean_commute, color=division))+
+#   geom_smooth(aes(y=mean_commute),method='loess')+
+#   geom_hline(aes(yintercept=median(gg_dat$mean_commute)))+
+#   scale_x_log10()
+
+ggplot(gg_dat, aes(x=dens, y=mean_commute, group=division))+
+  # geom_point(aes(color=division))+
+  geom_smooth(aes(y=mean_commute, color=division),se=F,method='loess')+
+
+  scale_x_log10()+
+  scale_color_manual(values=pal)+
+  labs(title="De-spaghettified, Trends are a Little Easier to See",
+       x="Population Density\nlog10 transformation", y="Mean Commute Time",
+       caption="viz by @WireMonkey\ncounty data: ACS 2015 County Data\nland area: USACS LND table\ntopic selection: @ThomasMock")+
+  theme_bw()+
+  facet_wrap(~division)
+
+sweet<-gg_dat %>% filter(mean_commute>23.5, mean_commute<24.5)
+
+ggplot(gg_dat, aes(x=dens, y=mean_commute, group=division))+
+  # geom_point(aes(color=division))+
+  geom_smooth(aes(y=mean_commute, color=division),se=F,method='loess')+
+  geom_vline(aes(xintercept=mean(sweet$dens, na.rm=T)))+
+  scale_x_continuous(trans="log",breaks=c(1,10,100,250,500,1000,10000,100000),
+                     labels=c("1", "10", "100", "250","500","1,000", "10,000", "100,000"))+
+  scale_color_manual(values=pal)+
+  labs(title="As Density Increases, Mean Commute Time Tends to Increase",
+       subtitle="However, there appears to be a sweet spot, around 225 people/square mile where mean commute time dips",
+       x="Population Density\nlog10 transformation", y="Mean Commute Time",
+       caption="viz by @WireMonkey\ncounty data: ACS 2015 County Data\nland area: USACS LND table\ntopic selection: @ThomasMock")+
+  theme_bw()+
+  theme(plot.caption = element_text(size=10) )
+
+
