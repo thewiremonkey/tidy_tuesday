76f4cbdd9378b353d5d901396c11a75107a5f159
TheWireMonkey <thewiremonkey@gmail.com>
2018-06-22 17:28:10 -0700

    the misc

---
+++ misc/BowieR.R
@@ -0,0 +1,97 @@
+library(spotifyr)
+library(geniusr)
+library(tidyverse)
+
+Sys.setenv(SPOTIFY_CLIENT_ID = 'e4ec23b2bada4263b05944323785c8ca')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '4dbb404760984bf2b98ff3c2ae1e6328')
+
+bowie<- get_artist_audio_features('David Bowie')
+
+bowie<-bowie %>%
+  mutate(year=lubridate::year(lubridate::ymd(bowie$album_release_year)))
+
+bowiecor<-bowie %>% select_if(is.numeric) %>%
+  mutate_if(is.character, as.factor)
+
+levels(as.factor(bowie$key))
+corrplot::corrplot(cor(bowiecor),type = "lower", order="hclust")
+
+library(rpart)
+bowietree<-rpart(track_popularity ~ ., bowiecor %>% select(-album_popularity))
+plot(bowietree)
+text(bowietree,  use.n = TRUE, all=FALSE)
+
+library(cluster)
+hclust(dist(bowiecor))
+
+
+# library(ape)
+library(cluster)
+library(dendextend)
+library(circlize)
+
+
+bowie_dend<-bowie %>% select(album_name, danceability, energy, key,speechiness, acousticness, instrumentalness, liveness, valence, tempo, album_popularity, mode)
+
+# bowie_dend<-bowie %>% select(album_name, key)
+
+bowie_full<-bowie_dend %>%
+  mutate(album_name=gsub( " *\\(.*?\\) *| *\\[.*?\\] *", "", album_name),
+         album_name=ifelse(grepl("Peter", album_name, ignore.case=TRUE), "Peter And The Wolf", album_name),
+         album_name=ifelse(grepl("Ziggy",album_name), "...Ziggy Stardust...", album_name) )%>%
+  group_by(album_name) %>%
+  summarise_if(is.numeric, mean, na.rm=TRUE) %>%
+  # filter(grepl("remastered", album_name, ignore.case=TRUE)) %>%
+  as.data.frame()
+
+rownames(bowie_full)<-bowie_full[,1]
+
+bowie_full<-bowie_full %>% select(-1)
+
+#make hierarchical cluster
+b_hc<-hclust(dist(bowie_full))
+
+# plot(as.phylo(b_hc),type="fan")
+
+
+# install.packages("dendextend")
+# install.packages("circlize")
+
+
+# create a dendrogram
+b_dend <- as.dendrogram(b_hc)
+
+# modify the dendrogram to have some colors in the branches and labels
+b_dend <- b_dend %>%
+  color_branches(k=8) %>%
+  color_labels(k=8)
+
+# plot the radial plot
+# par(mar = rep(6,6))
+circlize_dendrogram(b_dend, labels_track_height = 0.4,dend_track_height = .4)
+# circlize_dendrogram(b_dend, labels_track_height = NA, dend_track_height = .4)
+
+
+bowie<-bowie %>% select_if(is.numeric) %>%
+  bind_cols(bowie %>% select(album_name)) %>%
+  mutate(album_name=gsub( " *\\(.*?\\) *| *\\[.*?\\] *", "", album_name),
+         album_name=ifelse(grepl("Peter", album_name, ignore.case=TRUE), "Peter And The Wolf", album_name),
+         album_name=ifelse(grepl("Ziggy",album_name), "...Ziggy Stardust...", album_name) )%>%
+  group_by(album_name) %>%
+  summarise_if(is.numeric, mean, na.rm=TRUE) %>%
+  # top_n(20, album_popularity) %>%
+  ungroup() %>%
+  select(-album_popularity) %>%
+  column_to_rownames("album_name") %>%
+  as.data.frame()
+
+
+
+top_dend<-as.dendrogram(hclust(dist(bowie))) %>%
+  color_branches(k=8) %>%
+  color_labels(k=8)
+par(mar=c(0,5,0,5))
+circlize_dendrogram(top_dend,labels_track_height = 0.4,dend_track_height = .4)
+
+
+ggplot(bowie)
---
+++ misc/bowie shared on twitter.R
@@ -0,0 +1,55 @@
+library(spotifyr)#http://www.rcharlie.com/spotifyr/
+library(tidyverse)
+library(rpart)
+library(dendextend) #for colors and whatnot
+library(circlize) #https://stats.stackexchange.com/questions/4062/how-to-plot-a-fan-polar-dendrogram-in-r
+
+#follow http://www.rcharlie.com/spotifyr/ instructions for setting up a spotify account to get access to api.
+Sys.setenv(SPOTIFY_CLIENT_ID = 'e4ec23b2bada4263b05944323785c8ca')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '4dbb404760984bf2b98ff3c2ae1e6328')
+
+bowie<-if(!file.exists("bowie.rds")){
+  bowie<- spotifyr::get_artist_audio_features('David Bowie')
+  saveRDS(bowie, "bowie.rds")}else{
+    readRDS("bowie.rds")
+  }
+#there are a few options for the name, type the one you want in the console.
+
+#pull the numeric stats, rebind to the album names.  I didn't factorize the string #fields, such as mode and key because those are on the track level and I don't know how useful they'd be as means. Do some cleanup on the names--most of the Bowie albums on Spotify are "([year] Remastered)" Also Peter and the Wolf had several variants, as did Ziggy Stardust.
+
+hc_bowie<-bowie %>% select_if(is.numeric) %>%
+  bind_cols(bowie %>% select(album_name)) %>%
+  mutate(album_name=gsub( " *\\(.*?\\) *| *\\[.*?\\] *", "", album_name),
+         album_name=ifelse(grepl("Peter", album_name, ignore.case=TRUE), "Peter And The Wolf", album_name),
+         album_name=ifelse(grepl("Ziggy",album_name), "...Ziggy Stardust...", album_name) )%>%
+  group_by(album_name) %>%
+  summarise_if(is.numeric, mean, na.rm=TRUE) %>%
+  ungroup() %>%
+  column_to_rownames("album_name") %>%
+  as.data.frame()
+
+#create the clusters, make 'em pretty
+top_dend<-as.dendrogram(hclust(dist(hc_bowie))) %>%
+  color_branches(k=8) %>%
+  color_labels(k=8)
+
+#circlize  it
+circlize_dendrogram(top_dend,labels_track_height = 0.3,dend_track_height = .3)
+
+key_of_B<-bowie %>% group_by(key_mode) %>% 
+  count()
+
+ggplot(bowie, aes(x = valence, y = album_name)) + 
+  ggjoy::geom_joy() + 
+  ggjoy::theme_joy() +
+  ggtitle("Joyplot of Joy Division's joy distributions", subtitle = paste0("Based on valence pulled from Spotify's Web API with spotifyr"))
+#> Picking joint bandwidth of 0.112
+#
+kmeans_bowie<-bowie %>%select(-c(album_uri, track_uri, album_img)) %>% 
+  mutate_if(is.character, as.factor)
+
+kb<-kmeans_bowie %>% select(track_popularity, key_mode) %>% 
+  mutate(key_mode=as.numeric(key_mode))
+
+cluster<-kmeans(kb[,1:2],10)
+auggie_stardust<-broom::augment(cluster, bowie)
